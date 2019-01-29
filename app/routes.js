b8c6da1e5cfb04f0f136da8f53fbcd18ecc7fdf8
gazjoy <post@garethjoyce.co.uk>
2019-01-29 15:38:49 +0000

    Initial commit

--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,28 @@ router.get('/', function (req, res) {
 
 // add your routes here
 
+router.get('/details', function (req, res) {
+  res.redirect('details/check')
+})
+
+router.get('/verify', function (req, res) {
+  res.render('verify/1')
+})
+
+router.post('/nino', function (req, res) {
+  res.redirect('hub')
+})
+
+router.get('/calculator', function (req, res) {
+  res.redirect('/calculator/what-estimate')
+})
+
+router.post('/calculator/what-estimate', function (req, res) {
+  res.redirect('/calculator/when-retire')
+})
+
+router.post('/calculator/when-retire', function (req, res) {
+  res.redirect('/calculator/estimate')
+})
+
 module.exports = router
