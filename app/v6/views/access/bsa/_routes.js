820eed5897b65171106bf538a6e20a29a9de323b
gazjoy <post@garethjoyce.co.uk>
2020-04-03 12:33:52 +0100

    [JourAccess] BSA code journey

--- app/v6/views/access/bsa/_routes.js
+++ app/v6/views/access/bsa/_routes.js
@@ -25,4 +25,46 @@ router.post('/before', function (req, res) {
     }
 })
 
+router.post('/email', function (req, res) {
+    const answer = req.session.data['email'];
+    if (answer === 'no') {
+        res.redirect('email-not-found')
+    } else {
+        res.redirect('check-email2');
+    }
+})
+
+router.post('/code', function (req, res) {
+    const answer = req.session.data['code']
+    if (answer === 'no') {
+        res.redirect('code-expired')
+    } else {
+        res.redirect('email-confirmed');
+    }
+})
+
+router.post('/code2', function (req, res) {
+    const answer = req.session.data['code']
+    if (answer === 'no') {
+        res.redirect('code-expired2')
+    } else {
+        res.redirect('processing');
+    }
+})
+
+router.post('/nino', function (req, res) {
+    const answer = req.session.data['nino'],
+    match = ['QQ 12 34 56 C', 'QQ123456C', 'qq 12 34 56 c', 'qq123456c'];
+
+    if (match.includes(answer)) {
+        res.redirect('no-match');
+    } else {
+        res.redirect('check');
+    }
+})
+
+router.post('/set-email', function (req, res) {
+    res.redirect('check-email')
+})
+
 module.exports = router
